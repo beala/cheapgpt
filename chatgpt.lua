ed1c0f454ce9dc7aa1ebabc0418b6a53250d8388
Alex Beal <alexlbeal@gmail.com>
2023-03-09 22:52:14 -0700

    Add hammerspoon script.

---
+++ chatgpt.lua
@@ -0,0 +1,47 @@
+local prompt = "Rewrite the following text. Aim for clarity and succinctness. Avoid wordiness. It should sound professional but light.\n\n"
+
+-- Put your OpenAI API key in ~/.openai_key
+function getKey()
+    local home = os.getenv("HOME")
+    local file = io.open(home .. "/.openai_key", "r")
+    local contents = file:read("*all")
+    file:close()
+    return contents
+end
+
+function chatGpt(text, key, callback)
+    local headers = {
+        ["Content-Type"] = "application/json",
+        ["Authorization"] = "Bearer " .. key,
+    }
+    local post = {
+        model = "gpt-3.5-turbo",
+        messages = {
+            { role = "system", content = "You are a helpful assistant." },
+            { role = "user", content = text }
+        }
+    }
+    local postData = hs.json.encode(post)
+
+    hs.http.asyncPost("https://api.openai.com/v1/chat/completions", postData, headers, callback)
+end
+
+function getSelectedText()
+    local element = hs.uielement.focusedElement()
+    if element then
+        return element:selectedText()
+    end
+end
+
+-- Invoke API on key binding
+hs.hotkey.bind({ "cmd" }, "f1", function()
+    local text = getSelectedText()
+    if text then
+        chatGpt(prompt .. text, getKey(),function(_, body, _)
+            hs.eventtap.keyStroke({}, "delete")
+            hs.eventtap.keyStrokes(hs.json.decode(body)["choices"][1]["message"]["content"])
+        end)
+    else
+        hs.notify.new({ title = "Error", informativeText = "No text selected" }):send()
+    end
+end)
